117edc6aa288ce3d90ac098a0917479b20e0fe60
yunus <yunuspolat2528@gmail.com>
2023-09-04 12:07:31 +0200

    Pizzas app day2

--- In_Class/28_PIZZA_APP/pizzas/urls.py
+++ In_Class/28_PIZZA_APP/pizzas/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path,include
-from .views import home
+from .views import home,pizzas,order,my_orders,update,delete
 urlpatterns = [
-    path('', home, name="home")
+    path('', home, name="home"),
+    path('pizzas', pizzas, name="pizzas"),
+    path('order/<int:pk>', order, name="order"),
+    path('my_orders/', my_orders, name="myorders"),
+    path('update/<int:pk>', update, name="update"),
+    path('delete/<int:pk>', delete, name="delete"),
 ]
--- In_Class/28_PIZZA_APP/users/views.py
+++ In_Class/28_PIZZA_APP/users/views.py
@@ -11,15 +11,16 @@ def register(request):
         form=UserForm(request.POST)
         if form.is_valid():
             form.save()
+            messages.info(request,'register successfully')
             return redirect("home")
     context={
         'form' : form
     }
     return render(request, "users/register.html", context)
 
-#! Djangonun user icin default olusturdugu formu kullaniyoruz. in 6.line...
+#! Djangonun user icin default olusturdugu formu kullaniyoruz. in 9.line...
 
-
+from django.contrib import messages #!0109 #############
 
 def user_login(request):
     if request.method == 'POST':
@@ -27,6 +28,7 @@ def user_login(request):
         if form.is_valid():
             user = form.get_user()
             login(request, user)  # Do not forget to provide the user to the login function
+            messages.success(request,'login successfully')
             return redirect('home')
     else:
         form = AuthenticationForm()
@@ -38,4 +40,5 @@ def user_login(request):
 
 def user_logout(request):
     logout(request)
+    messages.warning(request,'logout successfully')
     return redirect('home')
